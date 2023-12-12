0a8eb6df10a38b893774cc9a337716160191736f
wwl.hmh <hmh@winwinlab.co.kr>
2023-12-12 18:28:34 +0900

    mongoose 전까지

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,22 @@
+// dotenv : 환경변수들을 파일에 넣고 사용 할 수 있게 해주는 개발용 모듈
+require('dotenv').config();
+
 const Koa = require('koa');
 const Router = require('koa-router');
 
 const app = new Koa();
 const router = new Router();
+const api = require('./api');
 
-router.get('/', (ctx, next) => {
-    ctx.body = '홈';
-});
+const mongoose = require('mongoose');
 
-app.use(router.routes());
-app.use(router.allowedMethods);
 
-app.listen(4000, () => {
-    console.log('heurm server is listening to port 4000');
+const port = process.env.PORT || 4000;
+
+router.use('/api', api.routes());
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(port, () => {
+    console.log('heurm server is listening to port ' + port);
 });
