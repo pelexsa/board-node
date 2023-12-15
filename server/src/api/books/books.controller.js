4362fa6e5a4151a64caafadfd20bc8ad63429f22
wwl.hmh <hmh@winwinlab.co.kr>
2023-12-15 16:19:36 +0900

    [add] 몽고db, 단일 조회 예외처리전까지

--- server/src/api/books/books.controller.js
+++ server/src/api/books/books.controller.js
@@ -1,8 +1,40 @@
 const Book = require('models/book');
 
-exports.list = (ctx) => {
-    ctx.body = 'listed';
+exports.list = async (ctx) => {
+    let books;
+
+    try {
+        books = await Book.find()
+        .sort({_id: -1})
+        .limit(3)
+        .exec(); //exec 호출 시, DB 실제 요청
+    } catch (e) {
+        return ctx.throws(500, e);
+    }
+
+    ctx.body = books;
 }
+
+exports.get = async (ctx) => {
+    const {id} = ctx.params;
+
+    let book;
+
+    try {
+        book = await Book.findById(id).exec();
+    } catch(e) {
+        return ctx.throw(500, e);
+    }
+
+    if (!book) {
+        ctx.status = 404;
+        ctx.body = {message: 'book not found'};
+        return;
+    }
+
+    ctx.body = book;
+};
+
 
 exports.create = async (ctx) => {
     const {
--- server/src/api/books/index.js
+++ server/src/api/books/index.js
@@ -4,13 +4,10 @@ const books = new Router();
 const booksCtrl = require('./books.controller');
 
 books.get('/', booksCtrl.list);
-
+books.get('/:id', booksCtrl.get);
 books.post('/', booksCtrl.create);
-
 books.delete('/', booksCtrl.delete);
-
 books.put('/', booksCtrl.replace);
-
 books.patch('/', booksCtrl.update);
 
 module.exports = books;
